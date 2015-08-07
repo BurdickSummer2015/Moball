598b7348ef39cce3c8edd5e20b8169561c56ce6b
Danny Weitekamp <dannyweitekamp@gmail.com>
2015-08-07 14:28:43 -0700

    added ip_settings and changed netconfig.sh and .cproject to work with it

--- netconfig.sh
+++ netconfig.sh
@@ -2,6 +2,10 @@
 #!/bin/bash
 # init
 
+#File name: netconfig.sh
+#Purpose: Sets up a static IP address with the the Phyflex.i.MX6, sets up ssh permissions between the Host and Phyflex board, and saves the the static IP in ip_settings so that the eclipse project can read it.
+
+
 function pause()
 {
    read -p "$*"
@@ -26,25 +30,39 @@ function valid_ip()
 
 
 echo 
+echo "NET WORK CONFIGURATION HAS STARTED!"
 echo "You will need a valid Static IP address inside your network to dedicate to to the Phyflex i.MX6. You should contact your network adminstrator to get one. If you have already gotten one, then you can enter it now. Or you can enter [d] to generate a list of available ip addresses on your ethernet network, and then pick one you like. But be warned, this method may open you up to IP address conflicts with other machines on your network."
 OK=0
-BoardStaticIP=0
+
+. ip_settings
 #Get the Host's IP address
 ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-while [ $OK -lt 1 ]; do
-  echo "Enter a valid Static IP:"
-  read BoardStaticIP
-  if (valid_ip $BoardStaticIP); then
-    echo "IP is valid: $BoardStaticIP"
-    OK=1
-  # elif ("$BoardStaticIP" == "d"); then
-  elif [ "$BoardStaticIP" == "d" ]; then
-  	#get your ip address
-  	sudo nmap -v -sn -n $ip/24 -oG - | awk '/Status: Down/{print $2}'
-  else
-    	echo "Invalid IP"
+
+skp=0
+if (valid_ip $BoardStaticIP); then
+ read -r -p "Would you like to use the previously configured IP: $BoardStaticIP [y/n]" response
+ if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  skp=1
+ fi
+fi
+if [ $skp -lt 1 ]; then
+ while [ $OK -lt 1 ]; do
+   echo "Enter a valid Static IP:"
+   read BoardStaticIP
+   if (valid_ip $BoardStaticIP); then
+     echo "IP is valid: $BoardStaticIP"
+     OK=1
+   # elif ("$BoardStaticIP" == "d"); then
+   elif [ "$BoardStaticIP" == "d" ]; then
+   	#get your ip address
+   	sudo nmap -v -sn -n $ip/24 -oG - | awk '/Status: Down/{print $2}'
+   else
+     	echo "Invalid IP"
   fi
-done
+ done
+fi
+
+
 printf "Configuring project files for Host: %s and Board: %s...\n" "$ip" "$BoardStaticIP"
 
 echo "Restart you Phyflex.iMX6 with minicom running on the Host and press m before the board counts down to 0 to enter the barebox menu. Then go to settings->network set your network settings on the board to the following:"
@@ -69,6 +87,8 @@ while [ $OK -lt 1 ]; do
   fi
 done
 
+sed -i "s/BoardStaticIP=.*/BoardStaticIP=$BoardStaticIP/g" ip_settings
+
 if [ -a "$HOME/.ssh/id_rsa.pub" ]; then
   echo "Public key found..."
 else
@@ -88,41 +108,23 @@ while [ $OK -lt 1 ]; do
   OK=1
  else
   echo
+  echo "WARNING!!!"
   echo "shh could not connect to root@$BoardStaticIP."
   echo "To resolve this issue open minicom on the board and run:"
   echo
   echo "ssh $(id -un)@$ip"
   echo
-  echo "This will add you to the list of known hosts on the board and ssh should work"
-  pause "After you are finished press any key to try to connect again..."
+  echo "This will add you to the list of known hosts on the board and ssh should work."
+  pause "Once you are done, press any key to try to connect again..."
  fi
 done
 
-read -r -p "Would you like to configure your project with Static IP: $BoardStaticIP? [y/n]" response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
- echo "editing .cproject..."
- sed -i "s/root@.*:/root@${BoardStaticIP}:/g" .cproject
-fi
+#read -r -p "Would you like to configure your project with Static IP: $BoardStaticIP? [y/n]" response
+#if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+# echo "editing .cproject..."
+# sed -i "s/root@.*:/root@${BoardStaticIP}:/g" .cproject
+#fi
 
 echo "Network configuration for Phyflex i.MX6 has finished."
 
-
-: <<'COMMENT'
-#get your ip address
-ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-sudo nmap -v -sn -n $ip/24 -oG - | awk '/Status: Down/{print $2}'
-  
-COUNTER=0
-while [  $COUNTER -lt 10 ]; do
-     	echo The counter is $COUNTER
-	pause 'Press [Enter] key to continue...'
-	sudo arp-scan -I wlan0 192.168.1.0/24
-     	let COUNTER=COUNTER+1 
-done
-COMMENT
-# ...
-# call it
-
-# rest of the script
-# ...
  
