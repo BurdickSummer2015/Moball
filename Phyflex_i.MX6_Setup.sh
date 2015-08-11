d40c7bbd0756ca3663f447a007ac06684ab478c2
labrat <labrat@labrat-Dimension-9100.(none)>
2015-08-11 14:09:29 -0700

    Added more instructions to Phyflex setup

--- Phyflex_i.MX6_Setup.sh
+++ Phyflex_i.MX6_Setup.sh
@@ -1,7 +1,7 @@
 
 #Welcome! If you are reading this then you probably want to set up a Phyflex iM.X.6 rapid development kit for development. I caution you, from start to finish the process will take at least two days. If you stray from this guide and follow only Phytec's Quickstart guide here: 
-#http://phytec.com/wiki/index.php?title=PhyFLEX-i.MX6_Linux_Quickstart-PD13.2.0#TFTP
-#if everything goes wrong, which it did for me, then process could take as long as 2 weeks. There are several things that are not mentioned in the quickstart guide that could really hold you back. To begin with in order to start off on the right foot you will need a couple things:
+#http://phytec.com/wiki/index.php?title=PhyFLEX-i.MX6_Linux_Quickstart-PD13.2.0
+#If everything goes wrong, which it did for me, then process could take as long as 2 weeks. There are several things that are not mentioned in the quickstart guide that could really hold you back. To begin with in order to start off on the right foot you will need a few things:
 
 #1) An RS-232(female) to USB(male) serial adapter. Be warned not all serial adapters are created equal. Some will will drop packets in both directions, and others will work just fine. They are almost all made in China and there is no accountability for the chips they are built with, so buy two or three differnet brands and at least one of them will work correctly. Paying more does not entail better quality. I payed $20 for one that crapped out and $7 for one that works flawlessly. 
 
@@ -17,13 +17,13 @@
 
 #Overview:
 #Before we start installing things. I'll explain all the pieces that the Host computer is responisble for. There are two parts that we need to build: The Toolchain, and the BSP. The Toolchain is what we are going to use to compile, link, and debug the C/C++ programs we are going to run on the board. And The BSP consists of 3 parts. First there is the barebox shell which is a shell you can open up to change settings and choose how the board will boot (a little bit like the BIOS on a regular computer). Second there is the Linux Kernel, which is you know Linux. And Third there is the root filesystem which is magical unicorns. Just kidding its just the filesystem for the board; folders and files an what not.
-#You should know that the Toolchain will take 30 minutes to 1 hour to build the first time (much longer on an old computer), and the BSP will take about 4-5 hours (or longer) to build the first time. If you change anything in the BSP, which you will probably need to do inorder to get the right drivers running on your board, then subsequent builds will only take a minute or two. Note that you will not need to rebuild the BSP just to rewrite programs for the board.
+#You should know that the Toolchain will take 30 minutes to 1 hour to build the first time (much longer on an old computer), and the BSP will take about 4-5 hours (or longer) to build the first time. If you change anything in the BSP, which you will probably need to do inorder to get the right drivers running on your board, then subsequent builds will only take a minute or two. Note that you will not need to rebuild the BSP if you simply want to write programs for the board.
 #Lucky for you simply running this program will install everything that you need and and build the BSP and toolchain for you. 
-#To get started make a folder on your computer that you are going to use to build everything. From now on we'll call it your "Phyflex" directory. Put it somewhere not too deep in your filesystem like in your home directory or Documents or Desktop. Make sure not to put your Phyflex directory inside a git/mercurial repository. This directory is going to be filled by several Gigabytes of data. When you start this setup script it will prompt you for your Phyflex Directory. Or if you prefer you can do the following:
+#To get started make a folder on your computer that you are going to use to build everything. From now on we'll call it your "Phyflex" directory. Put it somewhere not too deep in your filesystem like in your home directory or Documents or Desktop. Make sure not to put your Phyflex directory inside a git/mercurial repository. This directory is going to be filled by several Gigabytes of data. When you start this script it will prompt you for your Phyflex Directory. Or if you prefer you can do the following:
 
 #./Phyflex_i.MX6_Setup.sh -p=<path to your PhyflexDir>
 
-#When you are comfortable doing so, go ahead and run this file to get started. This will take a very long time to run, and you really ought to be aware of what it is doing, so please by all means read this file at your leisure. The important bits are thoroughly commented upon.
+#When you are comfortable doing so, go ahead and run this file to get started. This will take a very long time to run, and you really ought to be aware of what it is doing, so please by all means read this file at your leisure. The important bits are thoroughly commented upon.  As this runs it may periodically prompt you for your password. This should happen mostly at the beginning of the process, but just incase you should check in on its progress regularly. Although once it has begun building the BSP you may as well let it go over night.
 
 
 #Get the directory that this script is in and store it for later
@@ -136,12 +136,17 @@ sudo apt-get -y install build-essential
 #Install the eclipse-platform (which is barebones eclipse IDE) and the CDT package (which is the the C/C++ tool system)
 
 sudo apt-get -y install eclipse-platform eclipse-cdt
+
+#Install Minicom, which is the serial terminal you will use to communicate with the board.
+
 sudo apt-get -y install minicom
 
 #Install tftpd server. We use this to get our built images onto the board.
+
 sudo apt-get -y install tftpd-hpa
 
-#Erase /etc/default/tftpd-hpa and replace its contents with the following. The important part is that TFTP_DIRECTORY is set to where we built the images. Check tftp_setup.sh to see the code.
+#Erase /etc/default/tftpd-hpa and replace its contents. The important part is that TFTP_DIRECTORY is set to where we will build the images. Check tftp_setup.sh to see the code.
+
 sudo $DIR/scripts/tftp_setup.sh
 
 #If it doesn't already exist cd into ptxdist-2011.11.0 and install it
@@ -216,6 +221,8 @@ if [ "$SkipBSP" == "false" ]; then
 
  wget $wgetargs -O $PhyflexDir/BSP-Phytec-phyFLEX-i.MX6-PD13.2.3/src/sysstat-9.0.3.tar.gz 'http://repository.timesys.com/buildsources/s/sysstat/sysstat-9.0.3/sysstat-9.0.3.tar.gz'
 
+ #Install a few dependancies
+
 sudo apt-get -y install libxml-parser-perl lzop
 
  #Now build the BSP with ptxdist-2012.03.0 from the BSP directory and go home this will take several hours
@@ -248,15 +255,15 @@ fi
 #What you need to know to get started:
 
 #OK so we've installed a bunch of stuff, and we would like to know how to use them.
-#First and most importantly we need to use minicom. Minicom is a Serial Terminal. It is how you connect to the board via the RS-232/USB adapter you got earlier. Go ahead and connect the RS-232 end of the adapter to the UART 0 part of the board, and the USB end to your computer. Now run minicom in your terminal with:
+#First and most importantly we need to use minicom. Minicom is a Serial Terminal. It is how you connect to the board via the RS-232/USB adapter you got earlier. Go ahead and connect the RS-232 end of the adapter to the UART0 part of the board, and the USB end to your computer. Now run minicom in your terminal with:
 
 #sudo minicom
 
-#Now do CTRL+A, and then Z to open up the Minicom Command Summary. Then press O to Configure Minicom. Use the arrow keys to scroll down to "Serial port setup" and press Enter. The default configuration should be fine (115200 8N1, Hw Flow Yes, Sft Flow No) except that serial device should be changed to "/dev/ttyUSB0". Note that if you have other USB devices connected to you host machine then they may be hooked up to USB0 instead. So you may need to use "/dev/ttyUSB1". To change this just press "A" and then start typing. Then press Enter twice scroll down to "Save setup as dfl" and press Enter again. Then scroll down to "Exit" and press Enter. To Exit out of minicom press CTRL+A then X. Do this and then start minicom again with "sudo minicom". Note that if your host machine actually has an RS-232 port then you should use that instead of an adapter, and you probably won't need to alter the settings in minicom at all. 
+#Now do CTRL+A, and then Z to open up the Minicom Command Summary. Then press O to Configure Minicom. Use the arrow keys to scroll down to "Serial port setup" and press Enter. The default configuration should be fine (115200 8N1, Hw Flow Yes, Sft Flow No) except that serial device should be changed to "/dev/ttyUSB0". Just press "A" and then start typing. Note that if you have other USB devices connected to your host machine then they may be hooked up to USB0 instead. So you may need to use "/dev/ttyUSB1". When you are done press Enter twice scroll down to "Save setup as dfl" and press Enter again. Then scroll down to "Exit" and press Enter. To Exit out of minicom press CTRL+A then X. Do this and then start minicom again with "sudo minicom". Note that if your host machine actually has an RS-232 port then you should use that instead of an adapter, and in that case you probably won't need to alter the settings in minicom at all. 
 
-#Now that minicom is set up hook your ethernet cable into the board with the other end hooked into a modem. This should give the board sufficient power to boot up, and you should see the board booting in minicom. The board will also boot if you hook it into a power supply, but it will take significantly longer because it will wait as long as 2 minutes for an ethernet connection. I am not presently aware how to change this. After you see the big ASCII art "Phytec phyFlex-i.MX6" type in root to log on. If you are prompted for a password just press ENTER. 
+#Now that minicom is set up hook your ethernet cable into the board with the other end hooked into a modem. This should give the board sufficient power to boot up, and you should see the board booting in minicom. The board will also boot if you hook it into a power supply, but it will take significantly longer because it will wait as long as 2 minutes for an ethernet connection. I am not presently aware how to change this. After you see the big ASCII art "Phytec phyFlex-i.MX6" type in "root" to log on. If you are prompted for a password just press ENTER. 
 
-#Now we can setup eclipse. Run eclipse by typing "eclipse &" from the terminal. Either pick a new workspace or use the default one. Either way, if you got this code from a git repository, then you should have cloned this repository into your workspace folder. If you have not done this then go ahead and move the whole repository to your workspace folder now. If you have a working project then go ahead and use it otherwise follow the following instructions for making a new eclipse project.
+#Now we can setup eclipse. Run eclipse by typing "eclipse &" from the terminal. Either pick a new workspace or use the default one. Either way, if you got this code from a git repository, then you should have cloned this repository into your workspace folder. If you have not done this then go ahead and move the whole repository to your workspace folder now. If you have a working project then open it up and skip the next step. Otherwise follow the following instructions for making a new eclipse project.
 
 #First go to file->New->NewProject. Click C/C++ then C++ project. Click Next. Choose Hello World C++ Project and fill in Project Name. Then click Finish. Now from the Project Explorer right click your project and go to Properties. Go to C/C++ Build->Settings. In the Tool Settings tab fill in the Command: box for each entry as follows:
 #GCC C++ Compiler  ->  Command: arm-cortexa9-linux-gnueabi-g++
@@ -273,20 +280,33 @@ fi
 
 #scp "$HOME/.ssh/id_rsa.pub" "root@$BoardStaticIP:/home/ssh-keys/$(hostname)
 
-#where $BoardStaticIP is the static IP of your board. You should get a static IP from your network administrator to be safe. But you can always ping addresses in your network until you find an unused one. Although this will almost certainly cause issues later. Note that if you have netconfig.sh then you should simply use that, as it will do all of this automatically for you.
+#where $BoardStaticIP is the static IP of your board. You should get a static IP from your network administrator to be safe. But you can always ping addresses in your network until you find an unused one. But be warned this will almost certainly cause issues later. Note that if you have netconfig.sh then you should simply use that, as it will create a key pair for you, copy it onto the board, and resolve any conflicts automatically.
 
-#Once you have a static IP, or just an unsued one, and have correctly configured the network settings on your board as outlined in section 8.2.1 of the quickstart guide got to Project->Properties->C/C++ Build->Settings->Build Steps, and in Post-build steps, in the Command: line add the following:
+#Once you have a static IP, or just an unsued one, and have correctly configured the network settings on your board as outlined in section 8.2.1 of the quickstart guide go to Project->Properties->C/C++ Build->Settings->Build Steps, and in Post-build steps, in the Command: line add the following:
 
 #scp ./<name of project> root@<The IP address you chose>:/home/. 
 
-#And thats it for eclipse
+#And that's everything for eclipse
 
 #Now lets say that you want to add new drivers to your board. For example USB Serial drivers:
-#To do this we simply need to open a terminal cd into $PhyflexDir/BSP-Phytec-phyFLEX-i.MX6-PD13.2.3 and then run "ptxdist-2012.03.0 kernelconfig". Once it has loaded go to Device Drivers->USB Support then enable USB Serial Converter support with "y" then go into USB Serial Converter support and enable USB Serial Console device support, USB Generic Serial Driver, and FTDI Single Port Serial Driver. You could probably get it to work without USB Serial Console device and/or USB Generic Serial Driver, but this configuration worked for me.
+#To do this we simply need to open a terminal cd into $PhyflexDir/BSP-Phytec-phyFLEX-i.MX6-PD13.2.3 and then run "ptxdist-2012.03.0 kernelconfig". Once it has loaded go to "Device Drivers"->"USB Support" then enable "USB Serial Converter support" with "y" then go into "USB Serial Converter support" and enable "USB Serial Console device support", "USB Generic Serial Driver", and "FTDI Single Port Serial Driver". You could probably get it to work without USB Serial Console device and/or USB Generic Serial Driver, but this configuration worked for me.
 #Then exit the kernelconfig being sure to save your changes and run "ptxdist-2012.03.0 go" then "ptxdist-2012.03.0 images".
 #You can use a similar process to enable wifi on the board. This is outlined in section 10.2 of the Quickstart Guide
 
-#Whenever you reconfigure and rebuild the BSP you need to reflash the kernel and filesystem onto the board. The quickstart guide outlines ways of forgoing this step when you simply want to boot from TFTP and NFS servers. But since you will most likely not rebuild the BSP often you may as well flash your changes onto onboard memory. To do this, follow the directions outlined in section 9 of the Quickstart Guide. Note that the commands outlined in this section should be run in the barebox shell, which you can access by pressing "m" immediately after rebooting the board through minicom. You will probably only need to run the commands in section 9.2(kernel) and 9.3(filesystem), since section 9.1 involves flashing the barebox which you almost certainly will not need to alter.  
+#If you follow the wifi installation instructions and after running "ptxdist-2012.03.0 go" get an error involving "hostapd", then you can resolve it by running:
+
+#sudo touch /usr/local/bin/hostapd      ---> create file hostapd
+#sudo touch /usr/local/bin/hostapd_cli  ---> create file hostapd_cli
+#sudo chmod 777 /usr/local/bin/hostapd  ---> give permission to modify hostapd
+#sudo chmod 777 /usr/local/bin/hostapd_cli ---> give permission to modify hostapd_cli
+# Then relaunch the build of the BSP by running:  "ptxdist-2012.03.0 go" and then "ptxdist-2012.03.0 images"
+
+#Whenever you reconfigure and rebuild the BSP you need to reflash the kernel and filesystem onto the board. The Quickstart Guide outlines ways of forgoing this step when you simply want to boot from TFTP and NFS servers (Section 8.3). If you would like to persue this method you will need to install the NFS server (Section 4.1.2) as this script does not do it for you. But since you will most likely not rebuild the BSP often you may as well flash your changes onto onboard memory (or NAND). To do this, follow the directions outlined in section 9 of the Quickstart Guide. Note that the commands outlined in this section should be run in the barebox shell, which you can access by pressing "m" immediately after rebooting the board through minicom. You will probably only need to run the commands in section 9.2(kernel) and 9.3(filesystem), since section 9.1 involves flashing the barebox which you almost certainly will not need to alter.  
+
+#Specific instructions for using Vector Nav accelorometers:
+#The example code for the Vector Nav devices calls on two libraries that requires special compiler flags: -pthread and -lrt. To add these just go to Project->Properties->C/C++ Build->Tool Settings->GCC C++ Linker and in the all options box add: 
+
+#-pthread -lrt
 
 
 
